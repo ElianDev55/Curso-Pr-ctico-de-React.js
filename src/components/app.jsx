b63cdea082cd060207a04a0af3aec8082f7d3e8d
ELIAN <jevillaurrutia@gmail.com>
2023-01-06 08:58:15 -0500

    ARRGLO

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import '../styles/global.scss';
 
 const App = () => {
 	return (
-		<h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
+		<h1>Hola Mundo!</h1>
 	);
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,18 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
+		publicPath: '/'
 	},
 	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx'],
+		alias: {
+			'@components': path.resolve(__dirname, 'src/components/'),
+			'@containers': path.resolve(__dirname, 'src/containers/'),
+			'@styles': path.resolve(__dirname, 'src/styles/'),
+			'@icons': path.resolve(__dirname, 'src/assets/icons/'),
+			'@logos': path.resolve(__dirname, 'src/assets/logos/'),
+		}
 	},
 	module: {
 		rules: [
@@ -30,12 +38,16 @@ module.exports = {
 				]
 			},
 			{
-				test: /\.s[ac]ss$/i,
+				test: /\.(css|scss)$/,
 				use: [
 					"style-loader",
 					"css-loader",
 					"sass-loader",
 				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				type: 'asset'
 			}
 		]
 	},
@@ -48,4 +60,7 @@ module.exports = {
 			filename: '[name].css'
 		}),
 	],
+	devServer: {
+		historyApiFallback: true,
+	}
 }
