7725ddf91e77da5430faaeb7bcfc0eba3e0aeece
ELIAN <jevillaurrutia@gmail.com>
2023-01-08 15:36:55 -0500

    FINISH PROJECT E-COMERS WITH PLATZI

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -1,22 +1,30 @@
 import { useState } from "react";
-
 const initialState = {
-    cart: [],
+	cart: [],
 }
 
 const useInitialState = () => {
-    const [state, setState] = useState(initialState);
-    const  addToCart = (payload) => {
-        setState({
-            ...state,
-            cart: [...state.cart, payload],
-        });
-    }
-    return {
-        state,
-        addToCart,
-    }
-}
+	const [state, setState] = useState(initialState);
 
+	const addToCart = (payload) => {
+		setState({
+			...state,
+			cart: [...state.cart, payload]
+		});
+	};
+
+	const removeFromCart = (payload) => {
+		setState({
+			...state,
+			cart: state.cart.filter(items => items.id !== payload.id),
+		});
+	}
+
+	return {
+		state,
+		addToCart,
+		removeFromCart
+	}
+}
 
 export default useInitialState;
